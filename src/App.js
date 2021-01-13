338746f0359fd99433c6affd590bf82e5448b01c
myomyintkyaw2280 <myomyintkyaw2280@gmail.com>
2021-01-13 15:10:49 +0630

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import profile from './img/profile.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React App to Git Hub live</h1>
+      <img src={profile} alt="Profile Image" />
     </div>
   );
 }
